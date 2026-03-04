de390fe4b00f84ec3c35ebeb146e8f904d0a9a20
artemtimoshenko55-ux <artemtimoshenko55@gmail.com>
2026-03-04 21:48:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -883,7 +883,7 @@ async def ref50_handler(message: Message):
     used_cycles = get_ref_withdraw_count(user_id)
     available_cycles = active_refs // REQUIRED_ACTIVE_REFS
 
-    if available_cycles - used_cycles <= 0:
+    if available_cycles <= used_cycles:
         remaining = REQUIRED_ACTIVE_REFS - (active_refs % REQUIRED_ACTIVE_REFS)
         if remaining == REQUIRED_ACTIVE_REFS:
             remaining = REQUIRED_ACTIVE_REFS
@@ -1011,9 +1011,6 @@ async def admin_addref(message: Message):
     try:
         tg_id = int(parts[1])
         count = int(parts[2])
-        if count <= 0:
-            await message.answer('Кількість повинна бути більше 0')
-            return
     except:
         await message.answer("ID та кількість повинні бути числами.")
         return
@@ -1035,8 +1032,6 @@ async def admin_setref(message: Message):
     try:
         tg_id = int(parts[1])
         value = int(parts[2])
-        if value < 0:
-            value = 0
     except:
         await message.answer("ID та значення повинні бути числами.")
         return
