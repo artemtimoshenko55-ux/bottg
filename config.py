4c9241c19d3debf7a088a6e673f97feccdea4a61
artemtimoshenko55-ux <artemtimoshenko55@gmail.com>
2026-03-05 15:45:48 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,46 +1,12 @@
-# config.py
 
-# Токен бота от @BotFather
-BOT_TOKEN = "8372413837:AAF56J2g6D1omx6w9-kk64rx5N8ZmnY4dwA"
+BOT_TOKEN = "PUT_YOUR_BOT_TOKEN"
+REQUIRED_CHANNELS = ["@yourchannel"]
+PRIVATE_CHANNELS = {}
+ADMINS = [6305571895,1739423755]
 
-# Каналы для обязательной подписки (можно без @ или с @, или полной ссылкой)
-REQUIRED_CHANNELS = [
-    "-1001860574110",
-    "-1001560856411",
-    '-1001383662329',
-    "-1001194739066",
-    "-1002462551033",
-    "-1003371121986",
-    "-1002621308629",
-    "-1003590391093",
-]
+REF_BONUS = 0
+MIN_WITHDRAW = 0
+USD_RATE = 40
 
-PRIVATE_CHANNELS = {
-    "-1001860574110": "https://t.me/+k-NOcOYCQek1NWQy",
-    "-1001560856411": "https://t.me/+jTcK0sB1cndmYTcy",
-    "-1001383662329": "https://t.me/+8mjqocCWjAE3ZGMy",
-    "-1001194739066": "https://t.me/+SfOL9qT5sMo5ZTVi",
-    "-1002462551033": "https://t.me/vipchannelctypt",
-    "-1003371121986": "https://t.me/crypt7383",
-    "-1002621308629": "https://t.me/+EvyL2r-VLBkzNmUy",
-    "-1003590391093": "https://t.me/crypt7373",
-}
-
-# Сколько грн даём за одного активированного реферала
-REF_BONUS = 5.0
-
-# Минимальная сумма для вывода
-MIN_WITHDRAW = 60.0
-
-# Админы (сюда свой Telegram ID)
-ADMINS = [1428837532]
-
-FAKE_TOTAL_USERS = 0
-
-# Дата запуска бота (для статистики)
-BOT_START_DATE = "05.12.2025"
-
-# Примерный курс USD для отображения в $
-USD_RATE = 40.0  # 1$ ≈ 40 грн
-
-PAYOUTS_CHANNEL_URL = "https://t.me/earnmoney8301"
+BOT_START_DATE = "01.01.2024"
+PAYOUTS_CHANNEL_URL = ""
